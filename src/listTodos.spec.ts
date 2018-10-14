8b052cf157b20c33afa4ca50680bbe6ea1756d5a
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-10-14 16:44:30 -0700

    our first test, just to list the todos

---
+++ src/listTodos.spec.ts
@@ -0,0 +1,29 @@
+import { Browser } from 'puppeteer';
+import { resolve } from 'path';
+import * as faker from 'faker';
+
+import { getBrowser, initialize, screenshot } from '../util';
+
+let browser: Browser;
+
+beforeEach(async () => {
+  browser = await getBrowser();
+});
+
+afterEach(() => browser.close());
+
+describe('list todos', () => {
+  test('loads', async () => {
+    const page = await initialize(browser);
+
+    await page.waitForSelector('div[data-test-id]');
+    const todos = await page.$$('div[data-test-id]');
+
+    await screenshot(
+      page,
+      resolve(process.cwd(), './artifacts/todos-list.png')
+    );
+
+    expect(todos.length).not.toBe(0);
+  });
+});
