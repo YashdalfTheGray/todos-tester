a93fbc0840b2898f6b69212593ec18a531894e4c
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-10-21 13:07:44 -0700

    started adding a test for visibility"

--- src/listTodos.spec.ts
+++ src/listTodos.spec.ts
@@ -26,4 +26,20 @@ describe('list todos', () => {
     );
     await page.close();
   });
+
+  test('only shows open todos when toggled', async () => {
+    const { TEST_URL } = process.env;
+
+    const page = await openApp(browser, TEST_URL);
+    await page.waitForSelector('div[data-test-id]');
+
+    await page.click('[data-test-id="visibility-toggle"] input');
+    await page.$$('div[data-test-id]');
+
+    await screenshot(
+      page,
+      resolve(process.cwd(), './artifacts/open-todos-only.png')
+    );
+    await page.close();
+  });
 });
