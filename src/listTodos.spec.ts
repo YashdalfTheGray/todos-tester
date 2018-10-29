32c3ab062c7607caaf2a10d23a216e7edc51cc4a
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-10-29 07:56:18 -0400

    working tests, yess, with assertions too, yass

--- src/listTodos.spec.ts
+++ src/listTodos.spec.ts
@@ -1,7 +1,15 @@
 import { resolve } from 'path';
 import puppeteer from 'puppeteer';
 
-import { getBrowser, openApp, screenshot, setupEnvironment } from './util';
+import {
+  getAllTodos,
+  getBrowser,
+  getFirestore,
+  initFirebase,
+  openApp,
+  screenshot,
+  setupEnvironment
+} from './util';
 
 let browser: puppeteer.Browser;
 
@@ -9,37 +17,49 @@ jest.setTimeout(10000);
 
 beforeAll(async () => {
   setupEnvironment();
+  await initFirebase();
   browser = await getBrowser();
 });
 
-afterAll(async () => browser.close());
+afterAll(async () => {
+  const firestore = await getFirestore();
+  firestore.disableNetwork();
+  return browser.close();
+});
 
 describe('list todos', () => {
   test('loads', async () => {
     const { TEST_URL } = process.env;
+    const todos = await getAllTodos();
 
     const page = await openApp(browser, TEST_URL);
     await page.waitForSelector('div[data-test-id]');
+    const todosDisplayed = await page.$$('div[data-test-id]');
     await screenshot(
       page,
       resolve(process.cwd(), './artifacts/todos-list.png')
     );
+
+    expect(todosDisplayed).toHaveLength(todos.length);
     await page.close();
   });
 
   test('only shows open todos when toggled', async () => {
     const { TEST_URL } = process.env;
+    const filteredTodos = (await getAllTodos()).filter(t => !t.doneAt);
 
     const page = await openApp(browser, TEST_URL);
     await page.waitForSelector('div[data-test-id]');
 
     await page.click('[data-test-id="visibility-toggle"] input');
-    await page.$$('div[data-test-id]');
+    const displayedTodos = await page.$$('div[data-test-id]');
 
     await screenshot(
       page,
       resolve(process.cwd(), './artifacts/open-todos-only.png')
     );
+
+    expect(displayedTodos).toHaveLength(filteredTodos.length);
     await page.close();
   });
 });
