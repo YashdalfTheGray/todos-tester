477d4742f20f9865869d53034be6e6b28e04c2e7
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-11-23 13:02:25 -0500

    added more tests to the test suite"

--- src/todos.spec.ts
+++ src/todos.spec.ts
@@ -9,6 +9,7 @@ import {
   getAllTodos,
   getBrowser,
   getFirestore,
+  IFirebaseTodo,
   initFirebase,
   markTodoDone,
   markTodoUndone,
@@ -164,6 +165,19 @@ describe('todo', () => {
 });
 
 describe('add todo', () => {
+  function findAndDeleteNewlyAddedTodo(
+    newTodos: IFirebaseTodo[],
+    oldTodos: IFirebaseTodo[]
+  ) {
+    const todoDiff = newTodos.filter(t => oldTodos.includes(t));
+
+    if (todoDiff.length === 0) {
+      return;
+    }
+
+    return Promise.all(todoDiff.map(t => deleteTodo(t.id)));
+  }
+
   test('add dialog opens [@addtodo, @readonly]', async () => {
     const { TEST_URL } = process.env;
 
@@ -238,6 +252,62 @@ describe('add todo', () => {
     await screenshot(
       page,
       resolve(process.cwd(), './artifacts/invalid-add-todo-dialog.png')
+    );
+    await page.close();
+  });
+
+  test.skip('add dialog actually adds a todo [@addtodo]', async () => {
+    const { TEST_URL } = process.env;
+    const todos = await getAllTodos();
+
+    const page = await openApp(browser, TEST_URL);
+    await waitForNotLoading(page);
+    await page.waitForSelector('div[data-test-id]');
+    await page.waitForSelector('[data-test-id="add-todo"]');
+    await page.click('[data-test-id="add-todo"]');
+    await page.waitForSelector('[data-test-id="add-todo-dialog"]');
+    await page.waitForSelector('[data-test-id="add-todo-dialog-input"]');
+    await page.type(
+      '[data-test-id="add-todo-dialog-input"] input',
+      'this is a test todo'
+    );
+    await page.click('[data-test-id="add-todo-dialog-create-button"]');
+    await waitForNotLoading(page);
+
+    const newTodos = await getAllTodos();
+    expect(newTodos).toHaveLength(todos.length + 1);
+
+    await screenshot(
+      page,
+      resolve(process.cwd(), './artifacts/filled-add-todo-dialog.png')
+    );
+    await page.close();
+    await findAndDeleteNewlyAddedTodo(newTodos, todos);
+  });
+
+  test('add dialog cancel does not add a todo [@addtodo]', async () => {
+    const { TEST_URL } = process.env;
+    const todos = await getAllTodos();
+
+    const page = await openApp(browser, TEST_URL);
+    await waitForNotLoading(page);
+    await page.waitForSelector('div[data-test-id]');
+    await page.waitForSelector('[data-test-id="add-todo"]');
+    await page.click('[data-test-id="add-todo"]');
+    await page.waitForSelector('[data-test-id="add-todo-dialog"]');
+    await page.waitForSelector('[data-test-id="add-todo-dialog-input"]');
+    await page.type(
+      '[data-test-id="add-todo-dialog-input"] input',
+      'this is a test todo'
+    );
+    await page.click('[data-test-id="add-todo-dialog"] h2');
+    await page.click('[data-test-id="add-todo-dialog-cancel-button"]');
+
+    expect(await getAllTodos()).toHaveLength(todos.length);
+
+    await screenshot(
+      page,
+      resolve(process.cwd(), './artifacts/filled-add-todo-dialog.png')
     );
     await page.close();
   });
